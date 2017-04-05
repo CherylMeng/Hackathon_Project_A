0bce0020d350e94f3fedd23923ef63cfefbdcf78
Cheryl Meng <qingyi.meng@oracle.com>
2017-04-05 20:33:17 +0800

    Make changes to connect oracleDB & add .gitignore

--- ODP/Model/src/oracle/db/DBConnection.java
+++ ODP/Model/src/oracle/db/DBConnection.java
@@ -1,23 +1,16 @@
-package oracle.db; 
+package oracle.db;
 
 import java.sql.Connection;
-
-
-
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
- 
-import com.mysql.jdbc.Driver;          
+import java.sql.Statement;
 
 public class DBConnection {
 
 	public static void main(String[] args) {
 		System.out.println(getConnection());
+	    getUserRoles();
 	}
 
 
@@ -26,20 +19,44 @@ public class DBConnection {
 	 * @return
 	 */
 	public static Connection getConnection() {  
-				String forName ="com.mysql.jdbc.Driver";
-				String dbUrl = "jdbc:mysql://localhost:3306/fate";
-				String dbUser = "xiadai";
-				String dbPwd = "welcome1";
+				String forName ="oracle.jdbc.OracleDriver";
+				String dbUrl = "jdbc:oracle:thin:@slc09xzf.us.oracle.com:1521:epps";
+				String dbUser = "c##procurement";
+				String dbPwd = "group3";
 				Connection conn = null; 
 		try {
 			Class.forName(forName);
-			java.sql.DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			//java.sql.DriverManager.registerDriver(new com.oracle.jdbc.OracleDriver());
 			conn = DriverManager.getConnection(dbUrl, dbUser, dbPwd);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return conn;
 	}
+        
+        public static void getUserRoles(){
+            Connection conn = getConnection();
+            String sql = "SELECT * FROM USER_ROLE";
+            try{
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                if (rs != null) {
+                    try {
+                        while (rs.next()) {
+                            //Retrieve by column name
+                            int id = rs.getInt("ROLE_ID");
+                            String name = rs.getString("ROLE_NAME");
+                            System.out.println(id + " " + name);
+                            }
+                    } catch (SQLException se) {
+                        se.printStackTrace();
+                        }
+                    }
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+            
+        }
 
 	/**
 	 * 
